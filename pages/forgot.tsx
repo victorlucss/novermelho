4688e44fcbd04a75e3a0067516bc0016f532d7dd
Victor Lucas <victor.lucas@ifood.com.br>
2021-10-11 14:02:35 -0300

    feat: Forgot password page

---
+++ pages/forgot.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import { Box } from '@chakra-ui/react';
+
+import FormForgotPassword from '@Authentication/container/FormForgotPassword';
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <Box margin="10">
+      <FormForgotPassword />
+    </Box>
+  );
+}
